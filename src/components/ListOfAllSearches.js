9651ac8f3674ad304e36503717856e3ab5bef41d
Dominik <palacz.dominik@gmail.com>
2021-11-20 17:47:12 +0100

    Added eslint

--- src/components/ListOfAllSearches.js
+++ src/components/ListOfAllSearches.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const ListOdAllSearches = (list) => {
-    const greeting = list ? list : ['No history'];
-    const listItems = greeting.list.map((item) =>
-        <p>{JSON.stringify(item.ip)}</p>
-    )
-
-    return (<>
-        {listItems}
+  const greeting = list ? list : ["No history"];
+  const listItems = greeting.list.map((item, index) => (
+    <>
+      <p key={item.ip + index}>{JSON.stringify(item.ip)}</p>
     </>
-    )
-};
+  ));
 
+  return <>{listItems}</>;
+};
 
 export default ListOdAllSearches;
